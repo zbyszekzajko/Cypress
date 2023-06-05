a1ea59ab3596ad66b50b0161799b715b86f95158
zbyszekzajko <zbyszekzajko@gmail.com>
2023-06-05 22:25:37 +0200

    HTML reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+	reporter: "cypress-mochawesome-reporter",
 	e2e: {
 		viewportHeight: 1080,
 		viewportWidth: 1440,
@@ -13,6 +14,7 @@ module.exports = defineConfig({
 		projectId: "yy5uud",
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
+			require("cypress-mochawesome-reporter/plugin")(on);
 		},
 	},
 });
